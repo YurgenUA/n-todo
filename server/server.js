721d41a3af348736db6d6f62c3578b9c3abb4a90
YurgenUA <y.fenyuk@levi9.com>
2017-08-31 23:26:09 +0200

    added tests for POST method

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
+    console.log('----------------');
     let todo = new Todo({
         text: req.body.text
     });
@@ -23,3 +24,5 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
     console.log('started on port 3000');
 })
+
+module.exports = {app}
