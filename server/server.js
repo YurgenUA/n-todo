341849893a925a23abb24604c89aa5b7abc4a854
YurgenUA <y.fenyuk@levi9.com>
2017-09-05 21:21:34 +0200

    setup for heroku

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -50,8 +51,8 @@ app.get('/todos/:id', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('started on port 3000');
+app.listen(port, () => {
+    console.log(`started on port ${port}`);
 })
 
 module.exports = {app}
